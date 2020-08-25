295adeb1d399a5a688a96ef8c75bf26fa347648e
Renke-2020 <70155723+Renke-2020@users.noreply.github.com>
2020-08-25 20:43:12 +0530

    Update CsvReaderInjava.java

--- CsvReaderInjava.java
+++ CsvReaderInjava.java
@@ -16,7 +16,7 @@ public class CsvReaderInjava {
     public static void main(String... args) {
         List<Employee1> employees = readEmployeesFromCSV("employees.csv");
 
-        // let's print all the person read from CSV file
+        
         for (Employee1 emp : employees) {
             System.out.println(emp);
         }
@@ -26,29 +26,28 @@ public class CsvReaderInjava {
         List<Employee1> employees = new ArrayList<>();
         Path pathToFile = Paths.get(fileName);
 
-        // create an instance of BufferedReader
-        // using try with resource, Java 7 feature to close resources
+     
+       
         try (BufferedReader br = Files.newBufferedReader(pathToFile,
                 StandardCharsets.US_ASCII)) {
 
             // read the first line from the text file
             String line = br.readLine();
 
-            // loop until all lines are read
+            
             while (line != null) {
 
-                // use string.split to load a string array with the values from
-                // each line of
-                // the file, using a comma as the delimiter
+               
+  
                 String[] attributes = line.split(",");
 
                 Employee1 emp1 = createEmployee(attributes);
 
-                // adding book into ArrayList
+                
                 employees.add(emp1);
 
-                // read next line before looping
-                // if end of file reached, line would be null
+                
+              
                 line = br.readLine();
             }
 
@@ -64,7 +63,7 @@ public class CsvReaderInjava {
         String hireDate = metadata[1];
         String dept = metadata[2];
 
-        // create and return book of this metadata
+     
         return new Employee1(name, hireDate, dept);
     }
 
